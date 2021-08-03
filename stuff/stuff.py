7e6e4ad973c8a5aed10c8b6ea9b8e35ab1f49a21
Alex Goncalves <Alex.Goncalves@HABSMONMOUTH.ORG>
2021-08-03 22:41:10 +0100

    updating small bits that i forgot to change

--- stuff/stuff.py
+++ stuff/stuff.py
@@ -1,12 +1,15 @@
 import pygame
 import time 
+from tkinter import *   
 
 pygame.init()
 White = (230,230,230)
 Black = (0, 0, 0)
 Grey = (128, 128, 128)
 
-Width = 800
+paused = False
+Height = 1000
+Width = 1000
 cellThickness = 40
 lineThickness = 4
 totalColums = 0
@@ -14,7 +17,7 @@ totalRows = 0
 fatList = []
 rowList = []
 cellMap = []
-window = pygame.display.set_mode((Width,Width))
+window = pygame.display.set_mode((Width,Height))
 
 class Cell:
     def __init__(self, color, width, row, column, totalCol, totalRow, window):
@@ -61,33 +64,33 @@ class Cell:
         if self.alive == False:
             self.color = White
 
-    def getNeighbor(self, currentcell, row, column, totalRow, totalCol, list):
+    def getNeighbor(self, row, column, totalRow, totalCol, list):
 
 
         if not row + 1 > totalRow:
-             currentcell.neighbors.append(list[column][row + 1])
+             self.neighbors.append(list[column][row + 1])
 
         if not row - 1 < 0:
-             currentcell.neighbors.append(list[column][row - 1])
+             self.neighbors.append(list[column][row - 1])
 
         if not column + 1 > totalCol:
-             currentcell.neighbors.append(list[column+1][row])
+             self.neighbors.append(list[column+1][row])
 
         if not column - 1 < 0:
-             currentcell.neighbors.append(list[column-1][row])
+             self.neighbors.append(list[column-1][row])
 
         # diagonals -----------
         if not row + 1 > totalRow and not column + 1 > totalCol:
-            currentcell.neighbors.append(list[column + 1][row + 1])
+            self.neighbors.append(list[column + 1][row + 1])
 
         if not row + 1 > totalRow and not column - 1 < 0:
-            currentcell.neighbors.append(list[column-1][row+1])
+            self.neighbors.append(list[column-1][row+1])
 
         if not row - 1 < 0 and not column + 1 > totalCol:
-            currentcell.neighbors.append(list[column+1][row-1])
+            self.neighbors.append(list[column+1][row-1])
 
         if not row - 1 < 0 and not column - 1 < 0:
-            currentcell.neighbors.append(list[column-1][row-1])
+            self.neighbors.append(list[column-1][row-1])
 
     def getFriends(self):
         for friendCell in self.neighbors:
@@ -98,12 +101,11 @@ class Cell:
     def updateFriends(self):
         self.friends.clear()
 
-
 def drawLines(win, col, totalRows, totalColumns, thick, cellThick, windowW, windowH):
-    for i in range(0, totalColumns + 2):
+    for i in range(0, totalColumns + 1):
         pygame.draw.line(win, col, (((i * cellThick)), 0), (((i * cellThick)), windowH), thick) # draws line from the cell's x co-ord to the bottom
 
-    for i in range(0, totalRows + 2):
+    for i in range(0, totalRows + 1):
         pygame.draw.line(win, col, ((0, (i * cellThick))), ((windowW ,(i * cellThick))), thick) # draws line from the cell's y co-ord to the end of the screen
 
 def getRows(height, cellW):
@@ -115,8 +117,6 @@ def getCols(width, cellW):
 def useDN(cellMap):
     for row in cellMap:
         for cell in row:
-            if (len(cell.friends)) >= 1:
-                print("cell at row:" + str(cell.row) + " col:" + str(cell.column) + " has " + str(len(cell.friends)) + "friends")
             if cell.alive == True:
                 if len(cell.friends) >= 4:
                     cell.makeDead()
@@ -126,45 +126,49 @@ def useDN(cellMap):
                 if len(cell.friends) == 3:
                     cell.makeAlive()
 
-
 totalColums = getCols(Width, cellThickness) # gets total rows depending on the dimensions of the screen
-totalRows = getRows(Width, cellThickness)
+totalRows = getRows(Height, cellThickness)
 
+
+# main ================================================= main
 
 for col in range(0, totalColums + 1):
     for row in range(0, totalRows):
         newCell = Cell(Black, cellThickness, row, col, totalColums, totalRows, window) # creates new cells
         if len(fatList) == totalColums : # if the number of cells in this list is equal to the width of the map, add list to cellmap and clear for next row
             cellMap.append(fatList)
-            fatList = []# 
+            fatList = []
         fatList.append(newCell) # add cell to row
 
-
-# main ================================================= main
-
-cellMap[10][10].makeAlive()
 cellMap[10][11].makeAlive()
-cellMap[10][9].makeAlive()
-cellMap[9][11].makeAlive()
-cellMap[8][10].makeAlive()
-count = 0
-#for item in cellMap:
-#    for cell in item:
-#        cell.getNeighbor(cell, cell.row, cell.column, totalRows-1, totalColums-1, cellMap)
+cellMap[11][12].makeAlive()
+cellMap[12][10].makeAlive()
+cellMap[12][11].makeAlive()
+cellMap[12][12].makeAlive()
+
+for row in cellMap:
+    for cell in row:
+        cell.getNeighbor(cell.row, cell.column, totalRows-1, totalColums-1, cellMap)
 
 while True:
     for item in cellMap:
         for currentcell in item:
             currentcell.drawNode()
-            drawLines(window, Grey, totalRows, totalColums, lineThickness, cellThickness, Width, Width)
-            currentcell.getNeighbor(currentcell, currentcell.row, currentcell.column, totalRows-1, totalColums-1, cellMap)
+            drawLines(window, Grey, totalRows, totalColums, lineThickness, cellThickness, Width, Height)
             currentcell.updateFriends()
             currentcell.getFriends()
-    count = count + 1
     useDN(cellMap)
-    pygame.display.flip()
-    time.sleep(.1)
+    for event in pygame.event.get():
+        if event.type == pygame.K_SPACE:
+            paused = True
+            while paused == True:
+                print("burh")
+                for event in pygame.event.get():
+                    if event.type == pygame.K_SPACE:
+                        paused ==False
+
+    pygame.display.update()
 
 
-# to do list
-# fix problem, either neighbor list or friend list is not updating 
+pygame.quit()
+    
